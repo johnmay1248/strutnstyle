59336d6aa080b59aa9abd5b128f3119879f1379b
John May <johnmay1248@Johns-iMac.local>
2018-02-12 19:37:09 -0700

    for real added getters and setters

--- phpClasses/dbCon.php
+++ phpClasses/dbCon.php
@@ -47,6 +47,103 @@ class createCon
     public  $myconn;
 
     ########################################################################
+    # Properties. getters and setters                                      #
+    ########################################################################
+
+    /**
+     * Get property _host is the database host
+     *
+     * @return  string
+     */ 
+    public function get_host()
+    {
+        return $this->_host;
+    }
+
+    /**
+     * Set property _host is the database host
+     *
+     * @param  string  $_host  Property _host is the database host
+     *
+     * @return  self
+     */ 
+    public function set_host(string $_host)
+    {
+        $this->_host = $_host;
+
+        return $this;
+    }
+
+    /**
+     * Get property _user is the database user
+     *
+     * @return  string
+     */ 
+    public function get_user()
+    {
+        return $this->_user;
+    }
+
+    /**
+     * Set property _user is the database user
+     *
+     * @param  string  $_user  Property _user is the database user
+     *
+     * @return  self
+     */ 
+    public function set_user(string $_user)
+    {
+        $this->_user = $_user;
+
+        return $this;
+    }
+
+    /**
+     * Get property _pass no getter for password
+     *
+     * 
+     */ 
+
+
+    /**
+     * Set property _pass is the database user password
+     *
+     * @param  string  $_pass  Property _pass is the database user password
+     *
+     * @return  self
+     */ 
+    public function set_pass(string $_pass)
+    {
+        $this->_pass = $_pass;
+
+        return $this;
+    }
+
+    /**
+     * Get property _db is the database name
+     *
+     * @return  string
+     */ 
+    public function get_db()
+    {
+        return $this->_db;
+    }
+
+    /**
+     * Set property _db is the database name
+     *
+     * @param  string  $_db  Property _db is the database name
+     *
+     * @return  self
+     */ 
+    public function set_db(string $_db)
+    {
+        $this->_db = $_db;
+
+        return $this;
+    }
+
+    ########################################################################
     # Constructor                                                          #
     ########################################################################
     
@@ -66,7 +163,7 @@ class createCon
         } else {
             $this->myconn = $con;
             echo 'Connection established!';}
-        return $this->myconn;
+        #return $this->myconn;
     }
 
     public function close() {
